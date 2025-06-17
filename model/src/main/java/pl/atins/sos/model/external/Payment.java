69e7f908bc4d84ae8f3d7ba99813750c48d4b634
scrapyard123 <125703741+scrapyard123@users.noreply.github.com>
2025-06-17 20:36:12 +0200

    Add mocked library and payment services

---
+++ model/src/main/java/pl/atins/sos/model/external/Payment.java
@@ -0,0 +1,71 @@
+package pl.atins.sos.model.external;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Payment extends BaseExternalEntity {
+
+    private long studentId;
+    private BigDecimal amount;
+    private LocalDate paymentDate;
+    private String paymentMethod;
+    private String referenceCode;
+    private String status;
+    private String title;
+
+    public long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(long studentId) {
+        this.studentId = studentId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(LocalDate paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getReferenceCode() {
+        return referenceCode;
+    }
+
+    public void setReferenceCode(String referenceCode) {
+        this.referenceCode = referenceCode;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
