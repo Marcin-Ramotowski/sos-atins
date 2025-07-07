cf0a5d26bd909598e5e1444bb18f4ab5b6ea3a5f
Marcin-Ramotowski <marcin00.mr@gmail.com>
2025-07-07 20:11:42 +0200

    Implemented DAO for City model

--- data/src/main/java/pl/atins/sos/data/dao/CityDao.java
+++ data/src/main/java/pl/atins/sos/data/dao/CityDao.java
@@ -1,13 +1,12 @@
 package pl.atins.sos.data.dao;
 
-import pl.atins.sos.model.Address;
 import pl.atins.sos.model.City;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CityDao extends CrudDao<City> {
-    Optional<City> findByName(String name);
+    List<City> findByName(String name);
     Optional<City> findByCode(String code);
-    List<City> findByCountry(String countryCode);
+    List<City> findByCountryCode(String countryCode);
 }
